2318eca216f0228377ecb0f0950fe58de9708111
smazepaa <smazepa@kse.ord.ua>
2023-11-12 21:16:05 +0200

    added classes Electronics and Books

--- commerce-system/commerce-system.cpp
+++ commerce-system/commerce-system.cpp
@@ -36,18 +36,97 @@ public:
         price(other.price), quantityInStock(other.quantityInStock) {}
 
     // Move constructor
-    Product(Product&& other) //noexcept
-        : productId(move(other.productId)), name(move(other.name)), 
+    Product(Product&& other) noexcept
+        : productId(move(other.productId)), name(move(other.name)),
         price(move(other.price)), quantityInStock(move(other.quantityInStock)) {
 
         other.productId = 0;
         other.name = "";
         other.price = 0;
         other.quantityInStock = 0;
+    }
 };
 
+class Electronics : public Product {
+    string brand;
+    string model;
+    string powerConsumption;
+
+public:
+
+    Electronics(const int id, const string name, const int price, const int quantity, 
+        const string brand, const string model, const string powerConsumption)
+        : Product(id, name, price, quantity), brand(brand), model(model), 
+        powerConsumption(powerConsumption) {}
+
+    string getBrand() const {
+        return this->brand;
+    }
+
+    string getModel() const {
+        return this->model;
+    }
+
+    string getConsumption() const {
+        return this->powerConsumption;
+    }
+
+    // Copy constructor
+    Electronics(const Electronics& other)
+        : Product(other), brand(other.brand), model(other.model), 
+        powerConsumption(other.powerConsumption) {}
+
+    // Move constructor
+    Electronics(Electronics&& other) noexcept
+        : Product(move(other)), brand(move(other.brand)), 
+        model(move(other.model)), powerConsumption(move(other.powerConsumption)) {
+        other.brand = "";
+        other.model = "";
+        other.powerConsumption = "";
+    }
+};
+
+class Books : public Product {
+    string author;
+    string genre;
+    string isbn;
+
+public:
+
+    Books(const int id, const string name, const int price, const int quantity,
+        const string author, const string genre, const string isbn)
+        : Product(id, name, price, quantity), author(author), genre(genre),
+        isbn(isbn) {}
+
+    string getAuthor() const {
+        return this->author;
+    }
+
+    string getGenre() const {
+        return this->genre;
+    }
+
+    string getISBN() const {
+        return this->isbn;
+    }
+
+    // Copy constructor
+    Books(const Books& other)
+        : Product(other), author(other.author), genre(other.genre),
+        isbn(other.isbn) {}
+
+    // Move constructor
+    Books(Books&& other) noexcept
+        : Product(move(other)), author(move(other.author)),
+        genre(move(other.genre)), isbn(move(other.isbn)) {
+        other.author = "";
+        other.genre = "";
+        other.isbn = "";
+    }
+};
 
 int main()
 {
-    cout << "Hello World!\n";
+    
+    return 0;
 }
